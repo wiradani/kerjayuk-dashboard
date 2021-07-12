5b0b59a0fd2db5b156b879ca7f1914106952c780
Wiradani R <adhan432@gmail.com>
2021-07-12 13:31:26 +0700

    init

---
+++ src/layout/DashboardLayout.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="wrapper" :class="{ 'nav-open': $sidebar.showSidebar }">
+    <side-bar
+      :background-color="sidebarBackground"
+      short-title="KerjaYuk!"
+      title="KerjaYuk!"
+    >
+      <template v-slot:links>
+        <sidebar-item
+          :link="{
+            name: 'Dashboard',
+            icon: 'ni ni-tv-2 text-primary',
+            path: '/dashboard',
+          }"
+        />
+
+        <sidebar-item
+          :link="{
+            name: 'Position',
+            icon: 'ni ni-planet text-blue',
+            path: '/icons',
+          }"
+        />
+        <sidebar-item
+          :link="{
+            name: 'Departement',
+            icon: 'ni ni-pin-3 text-orange',
+            path: '/maps',
+          }"
+        />
+      </template>
+    </side-bar>
+    <div class="main-content" :data="sidebarBackground">
+      <dashboard-navbar></dashboard-navbar>
+
+      <div @click="toggleSidebar">
+        <!-- your content here -->
+        <router-view></router-view>
+        <content-footer v-if="!$route.meta.hideFooter"></content-footer>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import DashboardNavbar from "./DashboardNavbar.vue";
+import ContentFooter from "./ContentFooter.vue";
+
+export default {
+  components: {
+    DashboardNavbar,
+    ContentFooter,
+  },
+  data() {
+    return {
+      sidebarBackground: "vue", //vue|blue|orange|green|red|primary
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      if (this.$sidebar.showSidebar) {
+        this.$sidebar.displaySidebar(false);
+      }
+    },
+  },
+};
+</script>
+<style lang="scss"></style>
